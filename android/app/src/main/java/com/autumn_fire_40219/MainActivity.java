e84083f77a8515e01451e013e4cb9d80439f1214
Crowdbotics <team@crowdbotics.com>
2023-04-25 23:35:08 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/autumn_fire_40219/MainActivity.java
@@ -0,0 +1,15 @@
+package com.autumn_fire_40219;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "autumn_fire_40219";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "autumn-fire-40219",
+  url: "https://autumn-fire-40219.botics.co",
+  api: "https://autumn-fire-40219.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
